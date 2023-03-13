3fda06b95cbb61f2c50005676daec9bfbbcf14f4
macbookpro <aslmmon993@gmail.com>
2023-03-13 20:46:19 +0200

    added card view for bottom sheet fragment

--- app/src/main/java/com/example/tap_payment_task/MainActivity.kt
+++ app/src/main/java/com/example/tap_payment_task/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tap_payment_task
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        val newOverride = Configuration(newBase?.resources?.configuration)
+        newOverride.fontScale = 1.0f
+        applyOverrideConfiguration(newOverride)
     }
 }
