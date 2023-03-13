69b833f90e4684077fba7cac0a2f65a5b7975cf0
macbookpro <aslmmon993@gmail.com>
2023-03-13 17:17:03 +0200

    added multiple card payment types

--- app/src/main/java/com/example/tap_payment_task/features/tap_payment_bottomSheet/presentation/TapPaymentBottomSheet.kt
+++ app/src/main/java/com/example/tap_payment_task/features/tap_payment_bottomSheet/presentation/TapPaymentBottomSheet.kt
@@ -4,17 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tap_payment_task.R
+import com.example.tap_payment_task.databinding.TapPaymentBottomSheetContentBinding
+import com.example.tap_payment_task.features.tap_payment_bottomSheet.presentation.adapter.PaymentTypeData
+import com.example.tap_payment_task.features.tap_payment_bottomSheet.presentation.adapter.PaymentsTypesAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class TapPaymentBottomSheet : BottomSheetDialogFragment() {
 
-
+    lateinit var paymentsTypesAdapter: PaymentsTypesAdapter
+    lateinit var binding: TapPaymentBottomSheetContentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_modal, container, false)
+    ): View {
+        binding = TapPaymentBottomSheetContentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPaymentTypeList()
+        paymentsTypesAdapter = PaymentsTypesAdapter()
+        paymentsTypesAdapter.submitList(initPaymentTypeList())
+
+        binding.rvPaymentsType.adapter = paymentsTypesAdapter
+
+    }
+
+    private fun initPaymentTypeList(): MutableList<PaymentTypeData> {
+        return mutableListOf<PaymentTypeData>(
+            PaymentTypeData(
+                1,
+                ContextCompat.getDrawable(requireContext(), R.drawable.apple_pay)
+            ),
+            PaymentTypeData(
+                2,
+                ContextCompat.getDrawable(requireContext(), R.drawable.google_pay)
+            ),
+            PaymentTypeData(
+                3,
+                ContextCompat.getDrawable(requireContext(), R.drawable.apple_pay)
+            ),
+            PaymentTypeData(
+                4,
+                ContextCompat.getDrawable(requireContext(), R.drawable.google_pay)
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/tap_payment_task/features/tap_payment_bottomSheet/presentation/adapter/PaymentsTypesAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.tap_payment_task.features.tap_payment_bottomSheet.presentation.adapter
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tap_payment_task.R
+
+
+class PaymentsTypesAdapter :
+    ListAdapter<PaymentTypeData, PaymentsTypesAdapter.ViewHolder>(PaymentTypesComparator) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        return ViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.payment_type_item, parent, false)
+        )
+    }
+
+
+    object PaymentTypesComparator : DiffUtil.ItemCallback<PaymentTypeData>() {
+        override fun areItemsTheSame(
+            oldItem: PaymentTypeData,
+            newItem: PaymentTypeData
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: PaymentTypeData,
+            newItem: PaymentTypeData
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val paymentType = getItem(position)
+        with(paymentType) {
+            holder.itemView.findViewById<ImageView>(R.id.iv_payment_type).setImageDrawable(image)
+        }
+
+    }
+
+
+}
+
+data class PaymentTypeData(var id: Int? = null, var image: Drawable?)
